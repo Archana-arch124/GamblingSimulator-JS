91a479c0a69668ed691c61c1288589100de5019d
unknown <#archgowda124@gmail.com>
2021-02-09 20:42:18 +0530

    [Archana]Added:Gamblor win or loose

--- utility.js
+++ utility.js
@@ -2,4 +2,24 @@ class Utility{
 
     dailyStake = 100;
     everyDayBet = 1;
+    
+    check_WinORLose = () => {
+        return Math.floor(Math.random()* 2);
+    }    
+
+    main_Simulation = () => {
+
+        let Result = this.check_WinORLose();
+
+            if(Result == 1) {
+                this.dailyStake += 1;
+                console.log(`\n  GAMBLER WINS `)
+            }
+            if(Result == 0) {
+                this.dailyStake -= 1;
+                console.log(`\n  GAMBLER LoOSE `)
+            }
+    }
 }
+
+module.exports = new Utility();
